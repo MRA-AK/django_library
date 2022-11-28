dabb9e37edb781bbaf6d052e7c3ec10b1e6094fe
MRA-AK <mra1373@gmail.com>
2022-11-28 19:09:30 +0100

    Change category model verbose_name_plural

--- core/library/models.py
+++ core/library/models.py
@@ -31,6 +31,9 @@ class Category(CommonInfo):
     """Books category"""
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.name
 
