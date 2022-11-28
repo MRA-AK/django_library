4d7a29beef0602cc4898ca3e0485e2e9b4f3873e
MRA-AK <mra1373@gmail.com>
2022-11-28 16:18:24 +0100

    Create Author, Publisher, Category, Book and Borrow Models

--- core/library/models.py
+++ core/library/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+
+class CommonInfo(models.Model):
+    """Abstract base class for models common info"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Author(CommonInfo):
+    """Books author"""
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Publisher(CommonInfo):
+    """Books publisher"""
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Category(CommonInfo):
+    """Books category"""
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(CommonInfo):
+    name = models.CharField(max_length=255)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Borrow(CommonInfo):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    start_at = models.DateTimeField(default=now)
+    end_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.book.name
