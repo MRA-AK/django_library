fd85ae40d8fff66b326bbad4b568380f0a7e399c
Mohammad Reza Akbari <71519609+MRA-AK@users.noreply.github.com>
2022-11-28 19:33:05 +0100

    Create library models (#1)
    * Create Author, Publisher, Category, Book and Borrow Models
    
    * Make migrations
    
    * Register models in admin panel

--- core/library/admin.py
+++ core/library/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from .models import (
+    Author, Publisher,
+    Category, Book,
+    Borrow
+)
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Show Author model in django admin panel.
+    """
+    list_display = ["full_name"]
+    search_fields = ["full_name"]
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    """
+    Show Publisher model in django admin panel.
+    """
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Show Category model in django admin panel.
+    """
+    list_display = ["name"]
+    search_fields = ["name"]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """
+    Show Book model in django admin panel.
+    """
+    list_display = [
+        "name",
+        "publisher",
+    ]
+    search_fields = ["name", "authors", "publisher", "categories"]
+    list_filter = ["authors", "publisher", "categories"]
+
+
+@admin.register(Borrow)
+class BorrowAdmin(admin.ModelAdmin):
+    """
+    Show Borrow model in django admin panel.
+    """
+    list_display = [
+        "user",
+        "book",
+        "start_at",
+        "end_at"
+    ]
+    search_fields = ["user", "book", "start_at", "end_at"]
+    list_filter = ["user",]
