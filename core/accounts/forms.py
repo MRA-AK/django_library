f1aea157bcb20654e7d1e0867b13dd3be682ce4c
MRA-AK <mra1373@gmail.com>
2022-11-29 07:29:49 +0100

    Create user management system (Register, Login & Logout)

---
+++ core/accounts/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    """
+    Form for register new user.
+    """
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password1',
+            'password2',
+        ]
